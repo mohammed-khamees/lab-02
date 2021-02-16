45d0e19820e708c11aea6110ffa9ab1e66a95a7d
mohammed-khamees <mohammed.khamis1996@yahoo.com>
2021-02-16 20:27:04 +0200

    mustache template and flexbox

--- js/app.js
+++ js/app.js
@@ -1,49 +1,153 @@
 'use strict';
 
+let keywords = [];
+
+let gallary = [];
+
 $(document).ready(
-	$.ajax('./../data/page-1.json').then((data) => {
-		let keywords = [];
-
-		data.sort(function (a, b) {
-			return a.horns - b.horns;
+	$.ajax('./../data/page-2.json').then((data2) => {
+		data2.forEach((img) => {
+			let newImage = new Image(img);
+			if (!keywords.includes(newImage.keyword)) {
+				keywords.push(newImage.keyword);
+			}
+			if (!gallary.includes(newImage)) {
+				gallary.push(newImage);
+			}
 		});
+	}),
 
+	$.ajax('./../data/page-1.json').then((data) => {
 		data.forEach((img) => {
 			let newImage = new Image(img);
 			if (!keywords.includes(newImage.keyword)) {
 				keywords.push(newImage.keyword);
 			}
-			newImage.render();
+
+			if (!gallary.includes(newImage)) {
+				gallary.push(newImage);
+			}
+			newImage.toHtml();
 		});
 
 		keywords.forEach((keyword) => {
 			let category = $('option').first().clone();
 			category.attr('value', keyword);
 			category.text(keyword);
-			$('select').append(category);
+			$('.filter').append(category);
 		});
 
-		$('select').on('change', function () {
-			if ($(this).val() === 'default') {
-				$('.content').html('');
-				data.forEach((img) => {
-					let newImage = new Image(img);
-					if (!keywords.includes(newImage.keyword)) {
-						keywords.push(newImage.keyword);
-					}
-					newImage.render();
-				});
-			} else {
-				$('.content').html('');
-			}
-			let search = data.filter((item) => item.keyword === $(this).val());
-			search.forEach((item) => {
-				let searchImage = new Image(item);
-				searchImage.render();
-			});
-		});
+		imageFilter(data);
+		imageSort(data);
+		pagination();
+		search(data);
 	}),
 );
+
+function getData(page) {
+	$('.content').html('');
+	$.ajax(`./../data/page-${page}.json`).then((data) => {
+		data.forEach((img) => {
+			let newImage = new Image(img);
+			if (!keywords.includes(newImage.keyword)) {
+				keywords.push(newImage.keyword);
+				gallary.push(newImage);
+			}
+			newImage.toHtml();
+		});
+
+		$('.filter').html(`<option value="default">Filter by Keyword</option>`);
+		keywords.forEach((keyword) => {
+			let category = $('option').first().clone();
+			category.attr('value', keyword);
+			category.text(keyword);
+			$('.filter').append(category);
+		});
+
+		$('.sort').val('default');
+
+		imageFilter(data);
+		imageSort(data);
+	});
+}
+
+function imageFilter(data) {
+	$('.filter').on('change', function () {
+		if ($(this).val() === 'default') {
+			$('.content').html('');
+			data.forEach((img) => {
+				let newImage = new Image(img);
+				newImage.toHtml();
+			});
+		} else {
+			$('.content').html('');
+		}
+
+		let search = gallary.filter((item) => item.keyword === $(this).val());
+
+		search.forEach((item) => {
+			let searchImage = new Image(item);
+			searchImage.toHtml();
+		});
+	});
+}
+
+function imageSort(data) {
+	let defaultData = [...data];
+	$('.sort').on('change', function () {
+		if ($(this).val() === 'name') {
+			$('.content').html('');
+			data.sort((a, b) => {
+				return a.title.localeCompare(b.title);
+			});
+
+			data.forEach((img) => {
+				let newImage = new Image(img);
+				newImage.toHtml();
+			});
+		} else if ($(this).val() === 'horns') {
+			$('.content').html('');
+			data.sort((a, b) => {
+				return a.horns - b.horns;
+			});
+
+			data.forEach((img) => {
+				let newImage = new Image(img);
+				newImage.toHtml();
+			});
+		} else {
+			$('.content').html('');
+			defaultData.forEach((img) => {
+				let newImage = new Image(img);
+				newImage.toHtml();
+			});
+		}
+	});
+}
+
+function pagination() {
+	$('.btn1').on('click', function () {
+		getData(1);
+	});
+	$('.btn2').on('click', function () {
+		getData(2);
+	});
+}
+
+function search(data) {
+	$('.search').on('keyup', function () {
+		let photos = gallary.filter((item) =>
+			item.title.toLowerCase().includes($(this).val().toLowerCase()),
+		);
+
+		$('.content').html('');
+
+		photos.forEach((item) => {
+			let searchImage = new Image(item);
+			searchImage.toHtml();
+		});
+	});
+}
 
 function Image(img) {
 	this.image_url = img.image_url;
@@ -53,10 +157,8 @@ function Image(img) {
 	this.horns = img.horns;
 }
 
-Image.prototype.render = function () {
-	$('.content').append(`<div id="photo-template">
-                <h2 class="title">${this.title}</h2>
-                <img src=${this.image_url} alt=${this.title} class="img">
-                <p class='desc'>${this.description}</p>
-            </div>`);
+Image.prototype.toHtml = function () {
+	let template = $('#template-photo').html();
+	let image = Mustache.render(template, this);
+	$('.content').append(image);
 };
